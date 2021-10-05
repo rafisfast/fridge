ef6985609c6d7449c61dbd2cb5f5c58b2583cf08
rafisfast <tah18245393@students.wolvcoll.ac.uk>
2021-10-05 23:46:19 +0100

    added bounds

--- src/App.js
+++ src/App.js
@@ -1,80 +1,93 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { StrictMode, useCallback, useEffect, useRef, useState } from 'react';
 import {Stage, Layer, Rect, Circle} from 'react-konva';
 import './App.css';
 
+const clamp = (x,min,max) =>  {return Math.min(Math.max(x,min),max) }
+
 function App() {
   const [shapes,setShapes] = useState([{
-    id: 1,
+    id: "0",
     x: window.innerWidth,
     y: window.innerHeight,
     radius: 150,
-    stroke:'black'
+    stroke:'black',
+    isDragging: false
+    // isDragging: true;
   }])
   
-  const [style,setStyle] = useState({})
-  const [dragging,setDragging] = useState(0)
+  const [MoveData,setMoveData] = useState()
+  const [dragging,setDragging] = useState()
+  // click; dragging = id
+  // mousemove = (x = DeltaX) relative to stage clamped to stage
 
-  const onScroll = (event) => {
-    console.log('yes',window.screenLeft)
-  }
+    // const onMouseMove = (e) => {
+    //   console.log("moving")
+    //   if (MoveData) {
+    //     console.log(shapes)
+    //     const id = MoveData.target.id()
+    //     const s = shapes
+    //     const {x,y} = e
+    //     const shape = s[id]
+    //     shape.x = x
+    //     shape.y = y
+    //     setShapes(s)
+    //   }
+    //   // const id = MoveData.target.id()
+    //   // console.log(id,e)
+    //   // const s = shapes
+    //   // const shape = s[id]
+    //   // const [minX,maxX] = [shape.radius,(window.innerWidth*2) - shape.radius]
+    //   // const [minY,maxY] = [shape.radius,(window.innerHeight*2)- shape.radius]
+    //   // const {x,y} = e.target.getAbsolutePosition()
+    //   // shape.x = clamp(x,minX,maxX)
+    //   // shape.y = clamp(y,minY,maxY)
+    //   // console.log(clamp(e.evt.x,minY,maxY),"clamp")
+    //   // // math.clamp(z,x,y) math.min(z,x) or z > x math.max(z,y)
+    //   // // math.max(math.min(x,min),max)
+    //   // // Math.max(e.x,shape.radius)
+    //   // console.log(s[dragging])
+    //   // setShapes(s)
+    // }
 
-  const onMouseMove = (q) => {
-    if (dragging) {
-      console.log('moving')
-      const s = shapes
-      s.map((shape)=> {
-        console.log(shape.id,dragging)
-        if (s.id === dragging) {
-          console.log('changing')
-          // shape.x = 50;
-          // shape.y = 50;
-          // shape.x = Math.min(window.innerWidth + shape.radius,shape.x)
-          // shape.y = Math.min(window.innerHeight + shape.radius,shape.y)
-        }  
-      })
-      // if (x < window.innerWidth || x > window.innerWidth || y < window.innerHeight || y > window.innerHeight) {
-      // }
-      setShapes(s)
+  // useEffect(()=> {
+  //   console.log("set")
+  //   window.addEventListener('mousemove',onMouseMove);
+  //   return () => window.removeEventListener('mousemove',onMouseMove)
+  // },[MoveData,shapes])
+
+  // const onMouseDown = (e) => {
+  //   console.log("clicked")
+  //   const id = e.target.id()
+  //   const [mX,mY] = [e.target.x,e.target.y]
+  //   // setDragging(id)
+  //   const q = "Hehe"
+  //   setMoveData(e)
+  //   // console.log(MoveData)
+  // }
+
+  const dragBound = (s,p) => {
+    console.log(p)
+    return {
+      x: clamp(p.x,s,(window.innerWidth*2)-s),
+      y: clamp(p.y,s,(window.innerHeight*2)-s)
     }
-    // return () => window.removeEventListener(onMouseMove)
   }
-  const onDragStart = (e,element) => {
-    const {layerX} = e.evt
-    //console.log(e,layerX,'dragging')
-    // console.log(e)
-    setDragging(e.target.id())
-    console.log("started dragging",e.target.id())
-    // window.addEventListener('mousemove',onMouseMove)
-  }
-
-  const onDragEnd = (e) => {
-    console.log("stopped dragging")
-    setDragging()
-    // window.removeEventListener('mousemove',onMouseMove)
-  }
-
-  useEffect(
-    () => {
-      window.scrollTo(window.innerWidth/2,window.innerHeight/2)
-      window.addEventListener('scroll',onScroll);
-      window.addEventListener('mousemove',onMouseMove);
-      return () => {
-        window.removeEventListener('scroll',onScroll)
-        window.removeEventListener('mousemove',onMouseMove)
-      }
-    },[dragging]
-  )
-
+  
   return (
     <div className="App" style={{width:(window.innerWidth*2),height:(window.innerHeight*2)}}>
-      <Stage className="Stage" width={window.innerWidth*2} height={window.innerHeight*2} style={style} >
+      <Stage className="Stage" width={window.innerWidth*2} height={window.innerHeight*2}>
         <Layer>
           {/* <Circle x={150} y={150} stroke="black" radius={150} /> */}
           {shapes.map((shape)=> (
-            <Circle 
-            onDragStart={(e)=> onDragStart(e)}
-            onDragEnd={onDragEnd} 
+            //console.log("x",shape.x),
+            <Circle
+            _useStrictMode
+            //onMouseDown={onMouseDown}
+            // onDragStart={(e)=> onDragStart(e)}
+            // onDragEnd={onDragEnd}
+            // onDragMove={onDrageMove}
             draggable 
+            dragBoundFunc={(p)=>dragBound(shape.radius,p)}
             key={shape.id} 
             id={shape.id}
             x={shape.x} 
