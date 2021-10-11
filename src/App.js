a95f210e6f5f266e929bc10f736b3965d4a88e92
rafisfast <tah18245393@students.wolvcoll.ac.uk>
2021-10-11 20:52:37 +0100

    initiliaising tiles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const clamp = (x,min,max) =>  {return Math.min(Math.max(x,min),max) }
 
 function App() {
 
-    const [sizeX,sizeY]  = [3000,3000]
+    // const [sizeX,sizeY]  = [,3000]
     const [shape,setShape] = useState(
         {
             id:"0",
@@ -19,11 +19,26 @@ function App() {
     
     const [offset,setoffset] = useState({x:0,y:0})
     const [dragging,setdragging] = useState()
+    const [selected,setselected] = useState()
+    const [data,setdata] = useState()
 
-    const draw = () => {
+    useEffect(()=> {
+        console.log('set data')
+        window.devicePixelRatio = 1
+        const context = canvas.current.getContext('2d')
+        setdata(context.getImageData(0,0,canvas.current.width,canvas.current.height))
+    },[])
+    
+    const draw = (removeStroke) => {
         const {x,y,width,height,color} = shape
         console.log(x,y)
         const context = canvas.current.getContext('2d')
+        // context.clearRect(x,y,)
+        // console.log(data)
+        // context.putImageData(data[data.length-1],0,0)
+        
+        context.beginPath();
+
         context.fillStyle = color;
         context.fillRect(x, y, width, height);
     }
@@ -39,7 +54,14 @@ function App() {
             if (canvas.current) {
                 const context = canvas.current.getContext('2d')
                 const {x,y,width,height,color} = shape
-                context.clearRect(x,y,width,height)
+                if (data) {
+                    context.putImageData(data,0,0)
+
+                }
+                // context.clearRect(x,y,width,height)
+                // context.clearRect(x-2,y,2,height)
+                    // data.pop()
+                    // setdata([...data,context.getImageData(0,0,canvas.current.width,canvas.current.height)])
                 console.log('clearing')
             }
         }
@@ -47,13 +69,17 @@ function App() {
 
     const shapeToMouse = useCallback((e)=> {  
         console.log('moving')
+        // console.log(data)
+        const context = canvas.current.getContext('2d')
         const {x,y} = offset
         const mouseX = e.clientX + window.scrollX
         const mouseY = e.clientY + window.scrollY
         setShape({...shape,
-            x: clamp(shape.x + mouseX - x,0,sizeX - shape.width),
-            y: clamp(shape.y + mouseY - y,0,sizeY - shape.height)
+            x: shape.x + mouseX - x, //clamp(shape.x + mouseX - x,0,canvas.current.width - shape.width),
+            y: shape.y + mouseY - y//clamp(shape.y + mouseY - y,0,canvas.current.height - shape.height)
         })
+        // data.pop()
+        // setdata([...data])
     },[offset])
 
     useEffect(()=> {
@@ -66,25 +92,36 @@ function App() {
             canvas.current.addEventListener('mousemove',shapeToMouse)
         }
     },[offset,dragging])
+    
+    
+    useEffect(()=> {
+        console.log(selected)
+        if(dragging || selected !== true) {
+        }
+    },[selected,shape,dragging])
 
-    const DraggingStart = (e) => {
-        console.log('offset',e.clientX,e.clientY)
+    const onMouseDown = (e) => {
         const mouseX = e.clientX + window.scrollX
         const mouseY = e.clientY + window.scrollY
         if (mouseX <= shape.x + shape.width && mouseX >= shape.x && mouseY <= shape.y + shape.height && mouseY > shape.y) {
+            
             setoffset({x:mouseX,y:mouseY})
+            // setselected(true)
             setdragging(true)
+        } else {
         }
     }
 
-    const DraggingEnd = (e) => {
+    const onMouseUp = (e) => {
         console.log(shape)
         setdragging(false)
     }
 
     return (
         <div className="App">
-            <canvas width ={sizeX} height={sizeY} ref={canvas} className="layers" onMouseDown={DraggingStart} onMouseUp={DraggingEnd}>
+            <canvas style={{/*transformOrigin:'0 0',transform:'scale(0.5)'*/}} width ={window.innerWidth} height={window.innerHeight} ref={canvas} className="layers" onMouseDown={onMouseDown} onMouseUp={onMouseUp}>
+            </canvas>
+            <canvas style={{}} width ={window.innerWidth} height={window.innerHeight} className="layers" onMouseDown={onMouseDown} onMouseUp={onMouseUp}>
             </canvas>
         </div>
     );
