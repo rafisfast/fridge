cec782b7167f9affe28800185e2cfc4f5753e3fa
rafisfast <tah18245393@students.wolvcoll.ac.uk>
2021-10-16 19:01:56 +0100

    returned to using Konvajs due to ease of development

--- src/App.js
+++ src/App.js
@@ -1,202 +1,160 @@
-import React, {createContext, useCallback, useContext, useEffect, useRef, useState } from 'react';
+import React, { StrictMode, useCallback, useEffect, useReducer, useRef, useState } from 'react';
+import {Stage, Layer, Rect, Circle, Transformer}  from 'react-konva';
+import Konva from 'konva'
 import './App.css';
 
+Konva.pixelRatio = 1
+
 const clamp = (x,min,max) =>  {return Math.min(Math.max(x,min),max) }
-const BaggageContext = createContext()
-
-function Tile(props) {
-    
-    const canvas = useRef()
-    const [shape,setShape,scrolloffset,setscrolloffset,scale,setscale] = useContext(BaggageContext)
-    const [style,setstyle] = useState({})
-    // const [scale,setscale] = useState(1)
-
-    useEffect(()=> {
-        draw()
-        // const e = ['gesturechange','']
-        const context = canvas.current.getContext('2d')
-        context.save()
-        canvas.current.addEventListener('wheel',(e)=> {
-            e.preventDefault()
-        })
-        canvas.current.addEventListener('gesturestart',(e)=> {
-            console.log(e,'started')
-        })
-    },[])
-
-    const pinchMultiplier = 0.025
-    
-    const onGesture = (e) => {
-        e.preventDefault()
-        // pinch and zoom out from where you are pointing
-        // console.log(e,'changing',e.scale > 1,scale + (1 - e.scale),clamp((e.scale > 1 ? scale + (e.scale - 1) : scale - (1 - e.scale)),0.15,4))
-
-        // dragging should change scale to e.scale
-
-        setscale(clamp((e.scale > 1 ? scale + (e.scale - 1) * pinchMultiplier : scale - (1 - e.scale) * pinchMultiplier),0.15,4))
-        //e.scale > 1 ? e.scale * pinchMultiplier - scale : e.scale * pinchMultiplier + scale
-        // canvas.current.style = {}
-        // setstyle({transform:`scale(${scale},${scale}) translate(${e.clientX/2}px,${e.clientY/2}px)`})
-        // console.log(scale,style)
-        // setscrolloffset({x:scrolloffset.x - (scale * e.clientX) * scale,y: scrolloffset.y  - (scale * e.clientX)})
-        // setscale(e.scale < scale ? scale - e.scale * 0.01 : scale + e.scale * 0.01)
-        // setscale(scale+(e.scale * 0.05))
-    }
-
-    useEffect(()=> {
-        canvas.current.addEventListener('gesturechange',onGesture)
-        return ()=> {
-            canvas.current.removeEventListener('gesturechange',onGesture)
-        }
-    },[scale])
-
-    // console.log(props.id,'he')
-
-    const draw = () => {
-        const {x,y,width,height,color} = shape
-       // console.log(x,y)
-       const context = canvas.current.getContext('2d')
-       context.resetTransform()
-       context.scale(scale,scale)
-       //    console.log(context.current.scale)
-       
-       // context.clearRect(x,y,)
-       // console.log(data)
-       // context.putImageData(data[data.length-1],0,0)
-       
-       const {x: mx,y: my} = scrolloffset
-       // console.log(mx,my)
-       context.beginPath();
-       
-       // if i <= 10 = 0 else 10
-       
-       context.fillStyle = color;
-       context.fillRect(x + mx, y  + my, width, height);
-       // console.log(x + props.id*1000)
-    }
-    
-    useEffect(()=> {
-        draw()
-        return () => {
-            const context = canvas.current.getContext('2d')
-            // const {x: mx,y: my} = scrolloffset
-            context.clearRect(0,0,canvas.current.width/scale,canvas.current.height/scale)
-            context.scale(scale,scale)
-            // context.restore()
-        }
-    },[window,shape,scrolloffset,scale])
-    
-    return (
-        <canvas style={style} ref={canvas} width ={window.innerWidth} height={window.innerHeight}  className="layers">
-        </canvas>
-    )
-}
 
 function App() {
 
-    // const [sizeX,sizeY]  = [,3000]
-    const main = useRef()
+  const [sizeX,sizeY]  = [3000,3000]
+  const [size,setsize] = useState({width:sizeX,height:sizeY})
+  const [selected,setselected] = useState("")
+  const [dragging,setdragging] = useState()
 
-    const [dragging, setdragging] = useState()
-    const [offset, setoffset] = useState()
-    const [scrolloffset,setscrolloffset] = useState({x:0,y:0})
-    const [scale,setscale] = useState(1)
+  const scrollContainer = useRef()
+  //const [Size,setSize] = useState({width:document.documentElement.clientWidth*2,height:document.documentElement.clientHeight*2})
 
-    const [shape, setShape] = useState(
-        // temp shapes dictionary
-        {
-            id: "0",
-            x: 0,
-            y: 0,
-            width: 250,
-            height: 150,
-            color: 'green'
-        }
-    )
+  const [shapes,setShapes] = useState([{
+    id: "0",
+    x: 200,
+    y: 200,
+    width: 150,
+    height: 150,
+    stroke:'black',
+    isDragging: false
+    // isDragging: true;
+  }])
 
-    const onWheel = (e) => {
-        // onscroll handler
-        e.preventDefault()
-        // e.stopImmediatePropagation()
-        const {deltaX: dx, deltaY: dy} = e
-        const {x: mx, y: my} = scrolloffset
-        setscrolloffset({x:mx+dx,y:my+dy})
-        // console.log(mouseoffset)
+  const transformerRef = useRef()
+
+  useEffect(()=> {
+    if (selected) {
+      console.log(transformerRef)
+      transformerRef.current.nodes([selected])
+      transformerRef.current.getLayer().batchDraw()
     }
+  },[selected])
 
-    useEffect(()=> {
+  const select = (e) => {
+    console.log(e.target.getAbsolutePosition())
+    const id = e.target.id()
+    if (id) {
+      console.log(e.target)
+      setselected(e.target)
+    }
+    // console.log(e.target.id())
+    // setselected(id)
+    // setselected(e.target.id())
+  }
+  // const [stageSize,setStageSize] = useState()
+  const stage = useRef()
+  
+  useEffect(()=> {
+    // window.addEventListener('resize', function(event) {
+    //   setsize({width:sizeX,height:sizeY})
+    // })
+  })
 
-        document.addEventListener('wheel',onWheel)
+  const onDragMove = (e) => {
+    setdragging(e.target) 
+  }
 
-        return () => {
-            document.removeEventListener('wheel',onWheel)
-        }
+  const onDragEnd = (e) => {
+    if (dragging) {
+      const id     = e.target.id()
+      // const width  = e.target.width() 
+      // const height = e.target.height()
+      const x      = e.target.x()
+      const y      = e.target.y()
+      shapes[id].x = x
+      shapes[id].y = y
+      setShapes(shapes)
+      setdragging()
+    }
+  }
 
-    },[scrolloffset])
+  const scale = (window.innerWidth) / size.width
     
-    const onMouseDown = (e) => {
-        
-        if (dragging) {
-            setdragging(false)
-            return
-        }
+  const dragBound = (s,p,q) => {
+    console.log(s,q)
+   // s = s * scale
+    // console.log(stage,stage.current.width(),p.x)
+    const height = stage.current.height()
+    const width  = stage.current.width()
 
-        const mouseX = e.clientX / scale
-        const mouseY = e.clientY / scale
-        const [shapex,shapey] = [shape.x + scrolloffset.x,shape.y + scrolloffset.y]
+    // console.log(document.innerWidth)
 
-        // console.log(e.clientX,shape.x+scrolloffset.x,shape.x+scrolloffset.x+shape.width)
-        // console.log(e.clientX >= shape.x + scrolloffset.x && e.clientX <= shape.x + scrolloffset.x + shape.width)
-        console.log(shapex,shapey,shape.width,shape.height)
-
-        if (mouseX >= shapex && mouseX <= shapex + (shape.width * scale) && mouseY >= shapey && mouseY <= shapey + (shape.height * scale)) {
-            setoffset({x:mouseX,y:mouseY})
-            // setselected(true)
-            setdragging(true)
-        }
+    return {
+      x: clamp(p.x,s,width-s),
+      y: clamp(p.y,s,height-s)
     }
 
-    const shapeToMouse = useCallback((e)=> {  
-        //console.log('moving')
-        // console.log(data)
-        const {x,y} = offset
-        const mouseX = e.clientX / scale
-        const mouseY = e.clientY  / scale
-        setShape({...shape,
-            x: shape.x + mouseX - x, //clamp(shape.x + mouseX - x,0,canvas.current.width - shape.width),
-            y: shape.y + mouseY - y//clamp(shape.y + mouseY - y,0,canvas.current.height - shape.height)
-        })
-        // data.pop()
-        // setdata([...data])
-    },[offset,scrolloffset])
+  }
+  // const scale = size.width / 800
 
-    useEffect(()=> {
-        document.addEventListener('gesturechange',(e)=> {
-            // prevent zooming normally
-            e.preventDefault()
-            // console.log(e)
-        })
-    },[])
+  return (
+    <div ref={scrollContainer} className="App">
+      <Stage ref={stage} className="Stage" width={size.width} height={size.height} >
+        <Layer>
+          {/* <Circle x={150} y={150} stroke="black" radius={150} /> */}
+          {shapes.map((shape)=> (
+            //console.log("x",shape.x),
+            <Circle
+            _useStrictMode
+            onMouseDown={select}
+            onTap={select}
+            onDragMove={onDragMove}
+            //onMouseDown={onMouseDown}
+            // onDragStart={(e)=> onDragStart(e)}
+            // onDragEnd={onDragEnd}
+            // onDragMove={onDrageMove}
+            draggable
+            onDragEnd={onDragEnd}
+            onTransform={(e)=>console.log(e)}
+            // dragBoundFunc={(p)=>dragBound(shape.width,p,shape.height)}
+            key={shape.id} 
+            id={shape.id}
+            x={shape.x} 
+            y={shape.y} 
+            stroke={shape.stroke}
+            radius={shape.width} />
+          ))}
+          {selected !== undefined &&
+          <Transformer
+            ref={transformerRef}
+            rotateEnabled = {false}
+            enabledAnchors={['top-right','top-left','bottom-left','bottom-right']}
+           // onTransformEnd={onTransformEnd}
+            boundBoxFunc={(oldBox, newBox) => {
+              // limit resize
+              //selected.radius
+              if (newBox.width < 30 || newBox.height < 30) {
+                return oldBox;
+              }
+              
+              // console.log(newbo)
+              
+              const id    = selected.id()
+              const {x,y,height,width}  = newBox
 
-    useEffect(()=> {
-        main.current.removeEventListener('mousemove',shapeToMouse)
-        if (dragging) {
-            main.current.addEventListener('mousemove',shapeToMouse)
-        }
-    },[offset,dragging])
+              console.log(newBox,oldBox)
+             
+              shapes[id].width = width
+              shapes[id].height = height
+              
+              setShapes(shapes)
 
-    const onMouseUp = (e) => {
-        //console.log(shape)
-        setdragging(false)
-    }
-
-    return (
-        <div className="App" ref={main} onMouseUp={onMouseUp} onMouseDown={onMouseDown}>
-            <BaggageContext.Provider value={[shape, setShape, scrolloffset, setscrolloffset, scale, setscale]}>
-                <Tile />
-            </BaggageContext.Provider>
-        </div>
-    );
+              return newBox;
+            }}
+          />}
+        </Layer>
+      </Stage>
+    </div>
+  );
 }
+
 
 export default App;
